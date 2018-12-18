55eebf86214337f7975b4996adde0ea05ec40304
kamiprok <kamiprok@gmail.com>
2018-12-18 22:48:57 +0100

    dodano logowanie

--- index2.php
+++ index2.php
@@ -1,10 +1,16 @@
-<<<<<<< HEAD
+<?php session_start();
+if(!isset($_SESSION['UserData']['Username'])){
+header("location:login.php");
+exit;
+}
+?>
+
 <!doctype html>
 <html>
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Login</title>
+    <title>Index2</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="img/icon.ico" />
@@ -13,46 +19,11 @@
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
     <script type="text/javascript" src="myScript.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <script>
+    </script>
 </head>
-
 <body>
-    <form action='index.html' method='post'>
-        <div style='display:block; margin:50px; left:50px'>
-            <p>Login:</p>
-            <p><input type='text' name='user'></p>
-            <p>Password:</p>
-            <p><input type='password' name='password'></p>
-            <p><input type='submit' value='submit' class='button-zadania'></p>
-        </div>
-    </form>
+<p>Elegancko, jesteś zalogowany.</p>
+<p>Ta strona <<< index2.php >>> jest dostępna tylko dla zalogowanych użytkowników. Kliknij <a href="logout.php" class='button-zadania'>Wyloguj</a> żeby się wylogować.
 </body>
-=======
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Login</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="img/icon.ico" />
-    <link rel="icon" href="img/icon.png" type="image/png"/>
-    <link rel="stylesheet" type="text/css" href="styles.css">
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-    <script type="text/javascript" src="myScript.js"></script>
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-</head>
-
-<body>
-    <form action='index.html' method='post'>
-        <div style='display:block; margin:50px; left:50px'>
-            <p>Login:</p>
-            <p><input type='text' name='user'></p>
-            <p>Password:</p>
-            <p><input type='password' name='password'></p>
-            <p><input type='submit' value='submit' class='button-zadania'></p>
-        </div>
-    </form>
-</body>
->>>>>>> 2608dd2cb7a1ef3c3711e730b5a1692b90ed9750
 </html>
--- login.php
+++ login.php
@@ -1,22 +1,47 @@
-<<<<<<< HEAD
-<?php
-
-    $login = $_POST['login'];
-    $password = $_POST['pw'];
-
-    echo $login."<br>";
-    echo $password;
-
-
-=======
-<?php
-
-    $login = $_POST['login'];
-    $password = $_POST['pw'];
-
-    echo $login."<br>";
-    echo $password;
-
-
->>>>>>> 2608dd2cb7a1ef3c3711e730b5a1692b90ed9750
+<?php session_start();
+if(isset($_POST['Submit'])){
+$logins = array('admin' => 'admin');
+$Username = isset($_POST['Username']) ? $_POST['Username'] : '';
+$Password = isset($_POST['Password']) ? $_POST['Password'] : '';
+if (isset($logins[$Username]) && $logins[$Username] == $Password){
+$_SESSION['UserData']['Username']=$logins[$Username];
+header("location:index2.php");
+exit;
+} else {
+$msg="<span style='color:red'>Login: admin   Hasło: admin</span>";
+}
+}
 ?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="img/icon.ico" />
+    <link rel="icon" href="img/icon.png" type="image/png"/>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+    <script type="text/javascript" src="myScript.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <script>
+    </script>
+</head>
+<body>
+    <form action='' method='post' name='Login_Form'>
+        <div style='display:block; margin:50px; left:50px'>
+            <p>Login:</p>
+            <p><input type='text' name='Username'></p>
+            <p>Password:</p>
+            <p><input type='password' name='Password'></p>
+            <p><input type='submit' name='Submit' value='Login' class='button-zadania'></p>
+        </div>
+    </form>
+    <?php if(isset($msg)){?>
+    <p style='margin-left:50px'><?php echo $msg;?></p>
+    <?php } ?>
+</body>
+</html>
