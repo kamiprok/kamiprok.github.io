c0c8263d58b0ab153181f4250ecbc07eb20d9031
kamko <kamiprok@gmail.com>
2025-06-30 12:38:29 +0200

    added records

--- script.js
+++ script.js
@@ -10,12 +10,29 @@ const ownedAlbums = [
   { title: "Money for Nothing", artist: "Dire Straits", year: 1985, genre: "Pop Rock", cover: "https://upload.wikimedia.org/wikipedia/en/8/8c/Money_for_Nothing_single.JPG" },
   { title: "Greatest Hits", artist: "Queen", year: 1981, genre: "Rock", cover: "https://upload.wikimedia.org/wikipedia/en/0/02/Queen_Greatest_Hits.png" },
   { title: "Greatest Hits II", artist: "Queen", year: 1991, genre: "Rock", cover: "https://upload.wikimedia.org/wikipedia/en/3/38/Queen_-_Greatest_Hits_2.png" },
+  { title: "Dark Side of the Moon", artist: "Pink Floyd", year: 1973, genre: "Progressive Rock", cover: "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png" },
   { title: "Heart Of Glass I Inne Przeboje", artist: "Various", year: 1979, genre: "Pop", cover: "https://i.discogs.com/TdqmQrvuMsTK0k09kUCo7WChVfEcb-hmif_tohFXDo0/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTY0MDEz/MDgtMTQxODMzMjY3/My02NDk5LmpwZWc.jpeg" },
   { title: "Bad", artist: "Michael Jackson", year: 1987, genre: "Pop", cover: "https://upload.wikimedia.org/wikipedia/en/5/51/Michael_Jackson_-_Bad.png" },
-  { title: "Dark Side of the Moon", artist: "Pink Floyd", year: 1973, genre: "Progressive Rock", cover: "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png" },
+  { title: "Faith", artist: "George Michael", year: 1987, genre: "Pop", cover: "https://upload.wikimedia.org/wikipedia/en/9/9a/George_Michael_-_Faith.png" },
   { title: "True Blue", artist: "Madonna", year: 1986, genre: "Dance-Pop", cover: "https://upload.wikimedia.org/wikipedia/en/e/e0/Madonna_-_True_Blue_%28album_cover%29.png" },
   { title: "Greatest Hits", artist: "Shakin' Stevens", year: 1984, genre: "Rock and Roll", cover: "https://upload.wikimedia.org/wikipedia/en/c/c5/Shakin%27_Stevens_Greatest_Hits_1984.jpg" },
-  { title: "Faith", artist: "George Michael", year: 1987, genre: "Pop", cover: "https://upload.wikimedia.org/wikipedia/en/9/9a/George_Michael_-_Faith.png" }
+  { title: "Keeper of the Seven Keys Part 1", artist: "Helloween", year: 1987, genre: "power Metal", cover: "https://ecsmedia.pl/cdn-cgi/image/format=webp,/c/keeper-of-the-seven-keys-part-1-b-iext188290738.jpg" },
+  
+  { title: "Po prostu Krawczyk", artist: "Krzysztof Krawczyk", year: 2023, genre: "polski pop", cover: "https://ecsmedia.pl/cdn-cgi/image/format=webp,/c/po-prostu-krawczyk-antologia-przebojow-plyta-winylowa-b-iext153085930.jpg" },
+  { title: "Dusze kobiet", artist: "Zbigniew Wodecki", year: 1987, genre: "polski pop", cover: "https://ecsmedia.pl/cdn-cgi/image/format=webp,/c/dusze-kobiet-plyta-winylowa-b-iext177063358.jpg" },
+  { title: "Sun Machine", artist: "Myslovitz", year: 1996, genre: "polski rock", cover: "https://ecsmedia.pl/cdn-cgi/image/format=webp,/c/sun-machine-plyta-winylowa-b-iext153057356.jpg" },
+  { title: "Akustycznie - Suplement", artist: "Dżem", year: 1994, genre: "polski rock", cover: "https://ecsmedia.pl/cdn-cgi/image/format=webp,/c/akustycznie-suplement-digital-remastered-b-iext123230255.jpg" },
+  { title: "niemożliwe", artist: "Kwiat Jabłoni", year: 2019, genre: "polski pop", cover: "https://ecsmedia.pl/cdn-cgi/image/format=webp,/c/niemozliwe-b-iext185211309.jpg" },
+  { title: "sanah śpiewa Poezyje", artist: "sanah", year: 2022, genre: "polski pop", cover: "https://ecsmedia.pl/cdn-cgi/image/format=webp,/c/sanah-spiewa-poezyje-b-iext137248750.jpg" },
+
+  { title: "Manam", artist: "Manam", year: 1981, genre: "Rock", cover: "https://ecsmedia.pl/cdn-cgi/image/format=webp,/c/maanam-plyta-winylowa-b-iext182460865.jpg" },
+  { title: "Halina Kunicka", artist: "Halina Kunicka", year: 1966, genre: "polski pop", cover: "https://ecsmedia.pl/cdn-cgi/image/format=webp,/c/halina-kunicka-b-iext125074356.jpg" },
+  { title: "Urszula Czwarty Raz", artist: "Urszula", year: 1988, genre: "polski pop", cover: "https://winylmarket.com/filez/2020/08/R-2097521-1346260521-6657.jpeg.jpg" },
+  { title: "Rytmy Młodych", artist: "Various", year: 1965, genre: "polski pop", cover: "https://i.discogs.com/4bMcl2qCVneETfRkQYcIRShBDXlGt2i-MoWKs2dYCMY/rs:fit/g:sm/q:90/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTM5NDMz/ODktMTM1MDA0NDAz/NS0xMTAyLmpwZWc.jpeg" },
+  { title: "Powróćmy Jak Za Dawnych Lat", artist: "Various", year: 1982, genre: "polski pop", cover: "https://plytygramofonowe.pl/hpeciai/2b390c9831e91892ab5ed01b6711b649/pol_pl_Various-Piosenki-Z-Programu-Telewizyjnego-Powrocmy-Jak-Za-Dawnych-Lat-Hits-Of-The-Days-Gone-By-1982-Poland-Vinyl-52070_1.jpg" },
+  { title: "Dzieła Wszystkie", artist: "Fryderyk Chopin", year: 2010, genre: "Classical", cover: "https://i.discogs.com/t4shVHFxarx264iO-EoNMRxCQAqgWptWYvRY3ePx6Pc/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTYyMjYz/ODItMTQxNDM1NjA2/MC0zMzE0LmpwZWc.jpeg" },
+  { title: "Słynne Tanga i Walce", artist: "Various", year: 1965, genre: "Classical", cover: "https://i.discogs.com/5lFhr6nOQ1J8Q-PedEJ50TED1ptxehK78lKZVKeg_08/rs:fit/g:sm/q:90/h:520/w:511/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTI2MDYx/MzUtMTMwMTE2NjQ2/NC5qcGVn.jpeg" }
+  
 ];
 
 const wishlistAlbums = [
@@ -25,6 +42,7 @@ const wishlistAlbums = [
   { title: "Crazy World", artist: "Scorpions", year: 1990, genre: "Hard Rock", cover: "https://upload.wikimedia.org/wikipedia/en/f/f0/ScorpionsCrazyWorld.jpg" },
   { title: "The Rise and Fall of Ziggy Stardust", artist: "David Bowie", year: 1972, genre: "Glam Rock", cover: "https://upload.wikimedia.org/wikipedia/en/0/01/ZiggyStardust.jpg" },
   { title: "Led Zeppelin IV", artist: "Led Zeppelin", year: 1971, genre: "Hard Rock", cover: "https://upload.wikimedia.org/wikipedia/en/2/26/Led_Zeppelin_-_Led_Zeppelin_IV.jpg" },
+  { title: "Greatest Hits", artist: "Aerosmith", year: 1980, genre: "Hard Rock", cover: "https://upload.wikimedia.org/wikipedia/en/4/46/AerosmithGreatestHits.jpg" },
   { title: "Random Access Memories", artist: "Daft Punk", year: 2013, genre: "Electronic", cover: "https://upload.wikimedia.org/wikipedia/en/2/26/Daft_Punk_-_Random_Access_Memories.png" },
   { title: "Hotel California", artist: "Eagles", year: 1976, genre: "Rock", cover: "https://upload.wikimedia.org/wikipedia/en/4/49/Hotelcalifornia.jpg" },
   { title: "Thriller", artist: "Michael Jackson", year: 1982, genre: "Pop", cover: "https://upload.wikimedia.org/wikipedia/en/5/55/Michael_Jackson_-_Thriller.png" },
